3de7cf9b0d8420edb4025c1d662a04bc7c8b148b
adeeb boumoghlbey <adeebboumoghlbey@users.noreply.github.com>
2024-08-24 22:16:45 +0300

    Solve exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(s,n) {
+    let res = '';
+    for(let i = 0;i < n;i++){
+        res += s;
+    }
+    return res;
 };
 
 // Do not edit below this line
