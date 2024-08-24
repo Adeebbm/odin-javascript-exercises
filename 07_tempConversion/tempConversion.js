2d221d7d42f471da82436137ae6c92cecf691102
adeeb boumoghlbey <adeebboumoghlbey@users.noreply.github.com>
2024-08-24 23:13:13 +0300

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (5/9)*(f - 32);
+  return Math.floor(c) == c ? c : parseFloat(c.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = (9/5)*c + 32;
+  return Math.floor(f) == f ? f : parseFloat(f.toFixed(1));
 };
 
 // Do not edit below this line
